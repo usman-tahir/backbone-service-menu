6c095502694cabb5dbe1c6f1a4973f241084708a
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-04-03 21:18:48 -0400

    created the Backbone JS app skeleton

---
+++ js/app.js
@@ -0,0 +1,83 @@
+$(function () {
+  'use strict';
+
+  var Service = Backbone.Model.extend({
+    defaults: {
+      title: 'My Service',
+      price: 100,
+      checked: false
+    },
+
+    toggle: function () {
+      this.set('checked', !(this.get('checked')));
+    }
+  });
+
+  var ServiceList = Backbone.Collection.extend({
+    model: Service,
+
+    getChecked: function () {
+      return this.where({checked: true});
+    }
+  });
+
+  var ServiceView = Backbone.View.extend({
+    tagName: 'li',
+
+    events: {
+      'click': 'toggleService'
+    },
+
+    initialize: function () {
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render: function () {
+      this.$el.html('<input type="checkbox" value="1" name="' + this.model.get('title') + '" />' + this.model.get('title') + '<span>$' + this.model.get('price') + '</span>');
+      this.$('input').prop('checked', this.model.get('checked'));
+
+      return this;
+    },
+
+    toggleService: function () {
+      this.model.toggle();
+    }
+  });
+
+  var SERVICES = new ServiceList([
+    new Service({ "title": "Web Development", "price": 250 }),
+    new Service({ "title": "Web Design", "price": 250 }),
+    new Service({ "title": "Photography", "price": 100 }),
+    new Service({ "title": "Consultation", "price": 150 })
+  ]);
+
+  var APP = Backbone.View.extend({
+    el: $('#main'),
+
+    initialize: function () {
+      this.total = $('#total span');
+      this.list = $('#services');
+      // console.log(SERVICES);
+      this.listenTo(SERVICES, 'change', this.render);
+
+      SERVICES.each(function (service) {
+        var view = new ServiceView({model: service});
+        this.list.append(view.render().el);
+      }, this);
+    },
+
+    render: function () {
+      var total = 0;
+
+      _.each(SERVICES.getChecked(), function (element) {
+        total += element.get('price');
+        // console.log(total);
+      });
+
+      this.total.text('$' + total);
+      return this;
+    }
+  });
+
+  new APP();
+}());
